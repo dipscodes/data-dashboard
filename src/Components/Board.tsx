e8dbbaa75eecfea1160265b00cac89a0f102bd3f
VikramAditya <deepkumarkhamaru@gmail.com>
2023-06-15 16:12:16 +0530

    chart showing somehow

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -1,11 +1,53 @@
-import { useState } from "react";
+// @ts-nocheck
+
+import { useEffect, useState } from "react";
+import * as d3 from "d3";
+import LineChart from "./LineChart";
+
+
+function convertDateToISOFormat(json: object[], variable: string) {
+    const formattedJSON = json.map((entry) => {
+        const dateString: string = (entry as any).added;
+        var date = new Date(dateString);
+
+        var year = date.getFullYear();
+        var month = ("0" + (date.getMonth() + 1)).slice(-2);
+        var day = ("0" + date.getDate()).slice(-2);
+
+        var formattedDate = year + "-" + month + "-" + day;
+        const value = (entry as any)[variable];
+        return {date: new Date(formattedDate), value};
+    })
+    
+    return formattedJSON;
+}
 
 interface Props {
     name: string,
     type: string,
+    id: string,
 }
 
-export default function Board({name, type}: Props) {
+export default function Board({name, type, id}: Props) {
+    useEffect(() => {
+        (async () => {
+            const url = "http://localhost:5000/getData";
+            const apiResponse = await fetch(url);
+            const apiResponseInJSON = await apiResponse.json();
+            const formattedJSON = convertDateToISOFormat(apiResponseInJSON, "intensity");
+            console.log(formattedJSON);
+            await LineChart(formattedJSON, {
+                x: d => d.date,
+                y: d => d.value,
+                yLabel: "↑ Daily close ($)",
+                width: 1500,
+                height: 700,
+                color: "blue",
+                curve: d3.curveBasis,
+                id: id,
+            })
+        })();
+    });
     const box = {
         "graph": {
             width: "w-[80%]",
@@ -20,6 +62,6 @@ export default function Board({name, type}: Props) {
     const [boxData, setBoxdata] = useState(box);
     
     return (<div className={`${(boxData as any)[type].width} ${(boxData as any)[type].height} border-[rgba(47, 43, 61, 0.16)] border-solid border-2 rounded-md m-4`}>
-        {name}
+        <svg id={id}></svg>
     </div>);
 }
--- src/Components/Page.tsx
+++ src/Components/Page.tsx
@@ -1,10 +1,7 @@
 import Board from "./Board";
 
 export default function Page() {
-    return <div className="flex flex-wrap h-screen justify-center">
-    <Board name="hello" type="graph"></Board>
-    <Board name="world" type="pie"></Board>
-    <Board name="pie" type="pie"></Board>
-    <Board name="graph" type="graph"></Board>
-    </div>;
+    return (<div className="flex flex-wrap h-screen justify-center">
+        <Board name="hello" type="graph" id="linechart"></Board>
+    </div>);
 }
