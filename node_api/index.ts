a66895ca70d8afa379c09e514fc6653b18608b5b
VikramAditya <deepkumarkhamaru@gmail.com>
2023-06-15 12:44:27 +0530

    graph data end point working

--- node_api/index.ts
+++ node_api/index.ts
@@ -10,7 +10,33 @@ app.get('/', async (req, res) => {
     try {
         const allData = await Data.find({});
         res.send(allData);
-    } catch(error) {
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+app.get('/getData', async (req, res) => {
+    try {
+        const country = req.query.country;
+        const topic = req.query.topic;
+        const region = req.query.region;
+        const end_year = req.query.end_year;
+        const sector = req.query.sector;
+        const pestle = req.query.pestle;
+        const source = req.query.source;
+        const allData = await Data.find(Object.fromEntries(
+            Object.entries({
+                country,
+                topic,
+                region,
+                end_year,
+                sector,
+                pestle,
+                source,
+            }).filter(([_, value]) => value !== undefined)
+        ));
+        res.send(allData);
+    } catch (error) {
         res.send(error);
     }
 });
